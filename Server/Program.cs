51084f2b1c2a4a6f4fa1dc90805c76fb86b006c9
Jan Waltl <waltl.jan@gmail.com>
2018-07-12 14:50:00 +0200

    Server,Klient projekty

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
+using System.Net.Sockets;
 namespace Server
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 32653));
+
+			socket.Listen(1);
+			var s = socket.Accept();
+			byte[] buff = new byte[s.ReceiveBufferSize];
+			while (true)
+			{
+				int size = s.Receive(buff);
+				byte[] msg = new byte[size];
+				Array.Copy(buff, msg, size);
+				Console.WriteLine(Encoding.BigEndianUnicode.GetString(msg));
+			}
 		}
+
 	}
 }
