1eca617f1d38e85be43d949bd5351b1fd1dd2c54
Jan Waltl <waltl.jan@gmail.com>
2018-07-14 21:52:45 +0200

    Lepší návrh serveru

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
@@ -9,22 +10,78 @@ namespace Server
 {
 	class Program
 	{
-		static void Main(string[] args)
+		class ServerState
 		{
-			var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-			socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 32653));
+			public Socket listener;
+			public int numClients;
+		}
+		/// <summary>
+		/// Starts listening for incoming connections to the server.
+		/// </summary>
+		static async Task StarListening()
+		{
+			//TODO Error checking for this method
 
-			socket.Listen(1);
-			var s = socket.Accept();
-			byte[] buff = new byte[s.ReceiveBufferSize];
+			var endPoint = new IPEndPoint(IPAddress.Any, 23545);
+			ServerState serverState = new ServerState
+			{
+				listener = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp),
+				numClients = 0
+			};
+			serverState.listener.Bind(endPoint);
+			serverState.listener.Listen(5);
+
 			while (true)
 			{
-				int size = s.Receive(buff);
-				byte[] msg = new byte[size];
-				Array.Copy(buff, msg, size);
-				Console.WriteLine(Encoding.BigEndianUnicode.GetString(msg));
+				Console.WriteLine("Listening for a client...");
+				Socket client = await Task.Factory.FromAsync(serverState.listener.BeginAccept, serverState.listener.EndAccept, null);
+				//RESOLVe
+				_ = HandleClientAsync(client, serverState.numClients++);
 			}
 		}
-
+		/// <summary>
+		/// Handles newly connected client. 
+		/// </summary>
+		/// <param name="client">Incoming socket</param>
+		/// <param name="ID">ID of the client</param>
+		/// <returns>Task that completes when connections ends</returns>
+		static async Task HandleClientAsync(Socket client, int ID)
+		{
+			NetworkStream networkStream = new NetworkStream(client);
+			networkStream.ReadTimeout = 5000;
+			byte[] buffer = new byte[1024];
+			Console.WriteLine($"Connected to {ID}");
+			try
+			{
+				while (client.Connected)
+				{
+					int recieved = await networkStream.ReadAsync(buffer, 0, buffer.Length);
+					byte[] bMsg = new byte[recieved];
+					Array.Copy(buffer, bMsg, recieved);
+					string msg = Encoding.ASCII.GetString(bMsg);
+					if (msg == "quit")
+					{
+						client.Shutdown(SocketShutdown.Both);
+						client.Close();
+					}
+					Console.WriteLine($"{ID}:{msg}");
+				}
+				Console.WriteLine($"Disconnected from {ID}");
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Forced disconnection from {ID} because:");
+				Console.WriteLine(e.Message);
+			}
+			//TODO other exceptions?
+		}
+		static void Main(string[] args)
+		{
+			//RESOLVE
+			_ = StarListening();
+			System.Threading.Thread.Sleep(60000);
+		}
 	}
+
 }
+
