7d8af001189cb615472aa35b8c6e116dd240734b
Jan Waltl <waltl.jan@gmail.com>
2018-07-15 23:32:35 +0200

    Přidána Game třída implementující state machine pro hru.

---
+++ Client/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Client.GameStates;
+
+namespace Client
+{
+	/// <summary>
+	/// Encapsulases the game as a state machine.
+	/// </summary>
+	class Game
+	{
+		public enum States
+		{
+			Menu,
+			Connecting,
+			Spectaing,
+			Playing,
+		}
+		public Game(IGameState menuState)
+		{
+			states = new Dictionary<States, IGameState>();
+			states.Add(States.Menu, menuState);
+			activeState = menuState;
+		}
+		/// <summary>
+		/// Updates the game
+		/// </summary>
+		/// <param name="dt">delta time</param>
+		/// <returns>Whether the game wants to continue. <see langword="false"/> means exit.</returns>
+		public bool Update(double dt)
+		{
+			activeState = activeState.UpdateState(dt, states);
+			if (activeState == null)
+				return false;
+
+			Debug.Assert(states.ContainsValue(activeState));
+			return true;
+		}
+
+		IGameState activeState;
+		Dictionary<States, IGameState> states;
+	}
+}
---
+++ Client/GameStates/IGameState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.GameStates
+{
+	/// <summary>
+	/// Represent a state of the game's state machine.
+	/// </summary>
+	interface IGameState
+	{
+		/// <summary>
+		/// Updates the state.
+		/// </summary>
+		/// <param name="dt">delta time</param>
+		/// <param name="states">List of available states</param>
+		/// <returns>Next active state, must be from the list. So a new state must be added there.
+		/// Return null to exit the application.</returns>
+		IGameState UpdateState(double dt, Dictionary<Game.States, IGameState> states);
+	}
+}
