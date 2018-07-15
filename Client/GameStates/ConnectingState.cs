ee081286bcb47967c7a467c061b88340c3b8956a
Jan Waltl <waltl.jan@gmail.com>
2018-07-15 23:32:59 +0200

    Návrh tříd pro menu a připojování.

---
+++ Client/GameStates/ConnectingState.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+namespace Client.GameStates
+{
+	/// <summary>
+	/// GameState that represents the game connecting to the server
+	/// </summary>
+	class ConnectingState : IGameState, IDisposable
+	{
+		public ConnectingState(IPEndPoint serverAddress)
+		{
+			sAddress = serverAddress;
+			firstUpdate = true;
+		}
+
+		public void Dispose()
+		{
+			s.Dispose();
+		}
+
+		public IGameState UpdateState(double dt, Dictionary<Game.States, IGameState> states)
+		{
+			if (firstUpdate)//Connect to the server
+			{
+				firstUpdate = false;
+				s = new Socket(sAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				s.Connect(sAddress);
+
+				return this;
+			}
+			else if (s.Available > 0)
+			{
+				//TODO Collect data from the server
+				// - map, playerID, actions...
+				// - if collected, end the connection, return playing state
+			}
+
+			return this;
+		}
+
+		private bool firstUpdate;
+		private IPEndPoint sAddress;
+		private Socket s;
+	}
+}
---
+++ Client/GameStates/MenuState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Client.GameStates
+{
+	/// <summary>
+	/// Main menu of the game.
+	/// CURRENTLY it immidietely tries to connect to the server.
+	/// </summary>
+	class MenuState : IGameState
+	{
+		public IGameState UpdateState(double dt, Dictionary<Game.States, IGameState> states)
+		{
+			//Already was in menu
+
+			IPEndPoint serverAddress = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 23545);
+			ConnectingState state = new ConnectingState(serverAddress);
+			//Don't reuse
+			if (states.ContainsKey(Game.States.Connecting))
+				(states[Game.States.Connecting] as IDisposable)?.Dispose();
+
+			states[Game.States.Connecting] = state;
+			return state;
+		}
+	}
+}
