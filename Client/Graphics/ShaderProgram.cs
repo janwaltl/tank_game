c1711533b030d6b6c42b390f519187b67c38598f
Jan Waltl <waltl.jan@gmail.com>
2018-08-08 23:09:37 +0200

    ShaderProgram nyní umožnuje nastavovat uniforms + Bind/UnBind.

--- Client/Graphics/ShaderProgram.cs
+++ Client/Graphics/ShaderProgram.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
 using OpenTK.Graphics.OpenGL;
 
 namespace Client.Graphics
@@ -19,15 +20,35 @@ namespace Client.Graphics
 			GL.AttachShader(program, vShader);
 			GL.AttachShader(program, fShader);
 			GL.LinkProgram(program);
-			GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int status);
-			if (status != 0)
+			GL.GetProgram(program, GetProgramParameterName.LinkStatus, out int linkStatus);
+			if (linkStatus == 0)
 			{
 				string log = GL.GetProgramInfoLog(program);
-				//TODO throw
 				throw new ArgumentException("Program error: " + log);
 			}
+			GL.DetachShader(program, vShader);
+			GL.DetachShader(program, fShader);
+			GL.DeleteShader(vShader);
+			GL.DeleteShader(fShader);
+			activeUniforms = queryUniforms(program);
 		}
-
+		public void SetUniform(string name, Matrix4 mat)
+		{
+			if (!activeUniforms.TryGetValue(name, out Uniform u))
+				throw new ArgumentException($"'{nameof(name)}' is not an active uniform");
+			else if (u.type != ActiveUniformType.FloatMat4)
+				throw new ArgumentException($"'{nameof(name)}' is not {ActiveUniformType.FloatMat4} but {u.type}");
+			else
+				GL.UniformMatrix4(u.location, false, ref mat);
+		}
+		public void Bind()
+		{
+			GL.UseProgram(program);
+		}
+		public void UnBind()
+		{
+			GL.UseProgram(0);
+		}
 		private static int BuildShader(string source, ShaderType type)
 		{
 			int shader = GL.CreateShader(type);
@@ -35,14 +56,33 @@ namespace Client.Graphics
 			GL.ShaderSource(shader, source);
 			GL.CompileShader(shader);
 			GL.GetShader(shader, ShaderParameter.CompileStatus, out int status);
-			if (status != 0)
+			if (status == 0)
 			{
 				string log = GL.GetShaderInfoLog(shader);
 				throw new ArgumentException("Shader error: " + log);
 			}
 			return shader;
 		}
-
+		private static Dictionary<string, Uniform> queryUniforms(int program)
+		{
+			Dictionary<string, Uniform> uniforms = new Dictionary<string, Uniform>();
+			GL.GetProgram(program, GetProgramParameterName.ActiveAttributes, out int numAttribs);
+			GL.GetProgram(program, GetProgramParameterName.ActiveUniforms, out int numUniforms);
+			
+			for (int n = 0; n < numUniforms; ++n)
+			{
+				string name = GL.GetActiveUniform(program, n, out int size, out ActiveUniformType type);
+				int loc = GL.GetUniformLocation(program, name);
+				uniforms.Add(name, new Uniform { location = loc, type = type });
+			}
+			return uniforms;
+		}
+		struct Uniform
+		{
+			public int location;
+			public ActiveUniformType type;
+		}
+		readonly Dictionary<string, Uniform> activeUniforms;
 		readonly int program;
 	}
 }
